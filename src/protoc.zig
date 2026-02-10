9cdd3c9d1fd97598b1cafdd922079fd16d84bc3b
Arwalk <siragentprovocateurarwalk@gmail.com>
2026-02-10 19:46:28 +0100

    WIP

--- src/protoc.zig
+++ src/protoc.zig
@@ -1,69 +1,101 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const protobuf = @import("protobuf");
 
 pub const Protoc = struct {
     path: []const u8,
-};
 
-pub fn downloadProtocBinary(b: *std.Build) Protoc {
-    const os: ?[]const u8 = switch (builtin.os.tag) {
-        .macos => "osx",
-        .linux => "linux",
-        else => null,
-    };
+    pub fn downloadProtocBinary(b: *std.Build) Protoc {
+        const os: ?[]const u8 = switch (builtin.os.tag) {
+            .macos => "osx",
+            .linux => "linux",
+            else => null,
+        };
 
-    const arch: ?[]const u8 = switch (builtin.cpu.arch) {
-        .powerpcle, .powerpc64le => "ppcle",
-        .aarch64, .aarch64_be => "aarch_64",
-        .s390x => "s390",
-        .x86_64 => "x86_64",
-        .x86 => "x86_32",
-        else => null,
-    };
+        const arch: ?[]const u8 = switch (builtin.cpu.arch) {
+            .powerpcle, .powerpc64le => "ppcle",
+            .aarch64, .aarch64_be => "aarch_64",
+            .s390x => "s390",
+            .x86_64 => "x86_64",
+            .x86 => "x86_32",
+            else => null,
+        };
 
-    const dependencyName = if (builtin.os.tag == .windows)
-        try std.mem.concat(b.allocator, u8, &.{"protoc-win64"})
-    else if (os != null and arch != null)
-        try std.mem.concat(b.allocator, u8, &.{ "protoc-", os.?, "-", arch.? })
-    else
-        @panic("Platform not supported");
-    defer b.allocator.free(dependencyName);
+        const dependencyName = if (builtin.os.tag == .windows)
+            try std.mem.concat(b.allocator, u8, &.{"protoc-win64"})
+        else if (os != null and arch != null)
+            try std.mem.concat(b.allocator, u8, &.{ "protoc-", os.?, "-", arch.? })
+        else
+            @panic("Platform not supported:" ++ builtin.os.tag);
+        defer b.allocator.free(dependencyName);
 
-    if (b.lazyDependency(dependencyName, .{})) |dep| {
-        const path = if (builtin.os.tag == .windows) dep.path("bin/protoc.exe").getPath(b) else dep.path("bin/protoc").getPath(b);
+        if (b.lazyDependency(dependencyName, .{})) |dep| {
+            const path = if (builtin.os.tag == .windows) dep.path("bin/protoc.exe").getPath(b) else dep.path("bin/protoc").getPath(b);
+            return Protoc{ .path = path };
+        }
+
+        @panic("protoc dependency not found for platform: " ++ builtin.os.tag.name ++ " and architecture: " ++ builtin.cpu.arch.name);
+    }
+
+    pub fn findSystemProtoc() Protoc {
+        const name = switch (builtin.os.tag) {
+            .windows => Protoc{ .path = "protoc.exe" },
+            .linux, .macos => Protoc{ .path = "protoc" },
+            else => @panic("Platform not supported:" ++ builtin.os.tag),
+        };
+
+        const command = switch (builtin.os.tag) {
+            .windows => "where.exe",
+            .linux, .macos => "which",
+            else => @panic("Platform not supported:" ++ builtin.os.tag),
+        };
+
+        const result = std.ChildProcess.exec(.{
+            .allocator = std.heap.page_allocator,
+            .argv = &.{ command, name },
+        }) catch @panic("Failed to execute command: " ++ command);
+
+        if (result.exit_code != 0) {
+            @panic("Failed to find protoc: " ++ result.stderr);
+        }
+
+        const path = result.stdout.trim();
+
+        if (path.len == 0) {
+            @panic("Failed to find protoc: " ++ result.stderr);
+        }
+
         return Protoc{ .path = path };
     }
 
-    @panic("protoc dependency not found for platform: " ++ builtin.os.tag.name ++ " and architecture: " ++ builtin.cpu.arch.name);
+    pub fn buildProtocFromSources(b: *std.Build) Protoc {
+        const target = b.standardTargetOptions(.{});
+        const optimize = b.standardOptimizeOption(.{});
+        return Protoc{ .path = getProtocBin(b, target, optimize).path(b) };
+    }
+};
+
+
+fn getProtocDependency(
+    b: *std.Build,
+    target: ?std.Build.ResolvedTarget,
+    optimize: ?std.builtin.OptimizeMode,
+) *std.Build.Dependency {
+    return b.dependency("protobuf", .{ .target = target, .optimize = optimize });
 }
 
-pub fn findSystemProtoc() Protoc {
-    const name = switch (builtin.os.tag) {
-        .windows => Protoc{ .path = "protoc.exe" },
-        .linux, .macos => Protoc{ .path = "protoc" },
-        else => @panic("Platform not supported"),
-    };
+fn getProtocArtifact(
+    b: *std.Build,
+    target: ?std.Build.ResolvedTarget,
+    optimize: ?std.builtin.OptimizeMode,
+) *std.Build.Step.Compile {
+    return getProtocDependency(b, target, optimize).artifact("protoc");
+}
 
-    const command = switch (builtin.os.tag) {
-        .windows => "where.exe",
-        .linux, .macos => "which",
-        else => @panic("Platform not supported"),
-    };
-
-    const result = std.ChildProcess.exec(.{
-        .allocator = std.heap.page_allocator,
-        .argv = &.{ command, name },
-    }) catch @panic("Failed to execute command: " ++ command);
-
-    if (result.exit_code != 0) {
-        @panic("Failed to find protoc: " ++ result.stderr);
-    }
-
-    const path = result.stdout.trim();
-
-    if(path.len == 0) {
-        @panic("Failed to find protoc: " ++ result.stderr);
-    }
-
-    return Protoc{ .path = path };
+pub fn getProtocBin(
+    b: *std.Build,
+    target: ?std.Build.ResolvedTarget,
+    optimize: ?std.builtin.OptimizeMode,
+) std.Build.LazyPath {
+    return getProtocArtifact(b, target, optimize).getEmittedBin();
 }
